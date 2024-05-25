c7002fc8804a8a19bb9e781c918b91b1d3cdda34
Ryotruuser <ryonantruuser@gmail.com>
2024-05-25 14:35:07 -0300

    mais arrays

---
+++ Learning/arrays/atualizar-lista.js
@@ -0,0 +1,6 @@
+const listaEstudantes = ['joao', 'ana', 'caio', 'lara', 'marjorie', 'leo'];
+
+listaEstudantes.splice(1, 2, 'Rodrigo');
+// listaEstudantes.push('Rodrigo');
+
+console.log(listaEstudantes);
---
+++ Learning/arrays/dividir-arrays.js
@@ -0,0 +1,10 @@
+const listaEstudantes = ['Joao', 'Juliana', 'Ana', 'Caio', 'Lara',
+'Marjorie','Guilherme', 'Aline', 'Fabiana','Andre', 'Carlos',
+'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinicius', 'Renan', 'Renanta',
+'Daisy', 'Camilo'];
+
+const sala1 = listaEstudantes.slice(0, listaEstudantes.length / 2);
+const sala2 = listaEstudantes.slice(listaEstudantes.length/2);
+
+console.log(sala1);
+console.log(sala2);
