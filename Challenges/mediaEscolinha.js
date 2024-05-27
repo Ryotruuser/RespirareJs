44f38533a72bfb6d2d73b35b8dc408b1ee8c281e
Ryotruuser <ryonantruuser@gmail.com>
2024-05-27 19:16:25 -0300

    implementando o codigo que utiliza chaves dinamicas de pesquisa e percorre o objeto trazendo se as notas foram preenchidas

--- Challenges/mediaEscolinha.js
+++ Challenges/mediaEscolinha.js
@@ -6,31 +6,56 @@ const aluno = {
     turma: '2 - F',
     notas: 
     {
-        portugues: [],
+        portugues: [2, 5, 7],
         matematica: [],
         ciencias: [],
         historia: []
     }
 };
 
-while(true){
-    console.log("Portugues | Matematica | Ciencias | Historia | Sair \n")
-    let escolha = prompt("Digite a materia que deseja colocar a nota:").toLowerCase();
 
+
+// evitamos diversos if e else ifs pra cada materia com objeto usando um metodo de chave dinamica que consegui pensar por mim mesmo o.O estaria os estudos dando resultados
+while(true){
+    console.log("Opcoes do programa: Portugues | Matematica | Ciencias | Historia | Sair \n")
+
+    // Solicita ao usuário a matéria para a qual deseja adicionar uma nota
+    let escolha = prompt("Digite sua opcao: ").toLowerCase();
+
+    // Verifica se a matéria existe no objeto de notas do aluno
     if(aluno.notas[escolha]){
+
+        // Verifica se já foram inseridas 4 notas para a matéria
         if(aluno.notas[escolha].length >= 4){
                 
-            console.log("\nNotas das 4 unidades ja preenchidas.");
-            console.log(aluno.notas[escolha]);
-            break;
+            console.log(`\nNotas das 4 unidades ja preenchidas para a materia de ${escolha} \n`);
+
+            //percorre as materias do aluno e retona seus valores, se preenchidos ou nao. ty god for stackoverflow
+            for (let materia in aluno.notas) {
+                if(aluno.notas.hasOwnProperty(materia)) {
+                    console.log(`${materia}: ${aluno.notas[materia]} \n`);
+                }
+            }
 
         }else{
 
+            // Aqui usei um ternario para controlar a exibicao caso nao tenha nenhuma nota cadastrada ainda nao ficar so em branco
             console.log(`\nNotas atuais da materia do estudante: ${aluno.notas[escolha].length > 0 ? aluno.notas[escolha] : 'nenhuma nota cadastrada ainda.'}\n`);
-            let nota = parseInt(prompt(`Digite a ${aluno.notas[escolha].length + 1} nota do estudante: `));
+
+            // Solicita ao usuário a próxima nota
+            let nota = parseInt(prompt(`Digite a ${aluno.notas[escolha].length + 1} nota do estudante: \n`));
             aluno.notas[escolha].push(nota);
         }
+
     }else{
+
+        // se a escolha nao for materia pode ser que o usuario queira fechar a aplicacao ne.
+        if (escolha === 'sair'){
+            console.log("\nEncerrando...")
+            break;
+        }
+
+        // Informa ao usuário que a matéria não existe
         console.log("\nMateria nao existe na grade do aluno. \n");
     }
 };
