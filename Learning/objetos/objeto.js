0052abc7083a3f5a149327f10f736489470b1079
Ryotruuser <ryonantruuser@gmail.com>
2024-05-27 17:53:27 -0300

    iniciando com obj

---
+++ Learning/objetos/objeto.js
@@ -0,0 +1,9 @@
+const listaCPFs = ['1231231231232','2132131232132'];
+const infosPessoa = [['joao',[5,4,6,7],['12312312312','Joana']]]
+
+const objPessoa = {
+    nome: 'Renan Ramos',
+    notas: [6, 7, 9, 10]
+}
+
+console.log(objPessoa.nome)
