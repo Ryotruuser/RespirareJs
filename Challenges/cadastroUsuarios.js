4fa063cc39ba52775b5d9ca83b4dc0b980974805
Ryotruuser <ryonantruuser@gmail.com>
2024-05-31 20:15:54 -0300

    finalizado funcoes de apoio para o gerenciamento de usuarios

--- Challenges/cadastroUsuarios.js
+++ Challenges/cadastroUsuarios.js
@@ -1,33 +1,110 @@
+const fs = require('fs');
 const prompt = require("prompt-sync")({ sigint: true }); 
 
-// const usuario = {
-//     id: id,
-//     nome: nome,
-//     email: email,
-//     idade: idade,
-// }
+let usuarios = [
+    { id: 0, nome: 'aaa', email: 'aa', idade: 22 },
+    { id: 1, nome: 'bbb', email: 'bb', idade: 33 }
 
+];
 
-const usuarios = [];
-
-while(true){
-    console.log(`Gerenciador de usuarios`);
-    console.log(`
-    --------------------------
-    [1] Adicionar usuario 
-    [2] Remover usuario 
-    [3] Atualizar usuario 
-    [4] Exibir usuarios 
-    [5] Exportar usuarios 
-    [6] Importar Usuarios 
-    [0] Sair
-    --------------------------
-    `)
-    const opcao = parseInt(prompt("Digite a opcao desejada: "));
-    if(opcao == 0){
-        break;
+// Função para carregar usuários de um arquivo JSON
+function carregarUsuarios() {
+    if (fs.existsSync('usuarios.json')) {
+        const data = fs.readFileSync('usuarios.json', 'utf8');
+        usuarios = JSON.parse(data);
     }
+}
 
+// Função para salvar usuários em um arquivo JSON
+function salvarUsuarios() {
+    const data = JSON.stringify(usuarios, null, 2);
+    fs.writeFileSync('usuarios.json', data);
+}
 
+// Funcao para adicionar usuario na lista de usuarios
+function adicionarUsuario(){
+    const id = usuarios.length ? usuarios[usuarios.length - 1].id + 1 : 1; //revisar logica
+    const nome = prompt("Digite um nome: ");
+    const email = prompt("Digite um e-mail: ");
+    const idade = parseInt(prompt("Digite sua idade: "));
+
+    const  usuario = {id: id, nome: nome, email: email, idade: idade};
+    usuarios.push(usuario);
+    console.log("Usuario cadastrado com sucesso.");
+}
+
+// Funcao para remover usuario da lista usuarios
+function removerUsuario(){
+    const id = parseInt(prompt("Digite o id do usuario a ser removido: "));
+    usuarios.splice(id, 1);
+}
+
+// Funcao atualizar usuario na lista
+function atualizarUsuario(){
+    const id = parseInt(prompt("Digite o id do usuario a ser alterado: "));
+    let usuario;
+    for(usuario of usuarios){
+        if(usuario.id === id){
+            usuario = usuario;
+        }
+    }
+    console.log(`Informacoes atuais: ID: ${usuario.id} | NOME: ${usuario.nome} | EMAIL: ${usuario.email} | IDADE: ${usuario.idade}`);
+
+    console.log(`
+    -------------------------------------
+    Digite a propriedade a ser alterada 
+
+    [1] NOME 
+    [2] EMAIL 
+    [3] IDADE
+    [4] SAIR
+    -------------------------------------
+    `);
+    
+    const propASerAlterada = parseInt(prompt("Digite sua opcao: "));
+
+    if(propASerAlterada === 1){usuario.nome = prompt("Digite o novo nome: ")};
+    if(propASerAlterada === 2){usuario.email = prompt("Digite o novo email: ")};
+    if(propASerAlterada === 3){usuario.idade = parseInt(prompt("Digite a nova idade: "))};
+    if(propASerAlterada === 4){return};
 
 }
+
+// Funcao para exibir usuarios 
+function visualizarUsuarios(){
+    for(usuario of usuarios){
+        console.log(`
+        ID: ${usuario.id}
+        NOME: ${usuario.nome}
+        E-MAIL: ${usuario.email}
+        IDADE: ${usuario.idade}
+        `);
+    };
+}
+
+visualizarUsuarios()
+
+
+
+//loop do programa principal
+// while(true){
+//     console.log(`Gerenciador de usuarios`);
+//     console.log(`
+//     --------------------------
+//     [1] Adicionar usuario 
+//     [2] Remover usuario 
+//     [3] Atualizar usuario 
+//     [4] Exibir usuarios 
+//     [5] Exportar usuarios 
+//     [6] Importar Usuarios 
+//     [0] Sair
+//     --------------------------
+//     `)
+//     const opcao = parseInt(prompt("Digite a opcao desejada: "));
+//     if(opcao == 0){
+//         break;
+//     }
+
+
+
+// }
