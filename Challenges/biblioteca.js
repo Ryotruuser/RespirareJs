4fdbb0a7b27aeaac1dc7cc828e610632b335cd60
Ryotruuser <ryonantruuser@gmail.com>
2024-06-01 19:32:27 -0300

    implementacao do programa principal com as funcoes

--- Challenges/biblioteca.js
+++ Challenges/biblioteca.js
@@ -1,47 +1,47 @@
 const fs = require('fs');
 const prompt = require("prompt-sync")({ sigint: true }); 
 
-let livros = [{id: 0, titulo: "Harry Potter", autor: "J.K ROWLING", ano: 1997, disponivel: false},
-{id: 1, titulo: "Harry Potter 2", autor: "J.K ROWLING", ano: 1997, disponivel: true},
-{id: 2, titulo: "Harry Potter 3", autor: "J.K ROWLING", ano: 1997, disponivel: false}];
+let livros = [];
 
 
 //loop do programa principal
 
 //loop do programa principal
-// while(true){
-//     console.log(`Biblioteca JS`);
-//     console.log(`
-//     --------------------------
-//     [1] Adicionar Livro 
-//     [2] Remover Livro 
-//     [3] Atualizar Livro 
-//     [4] Exibir Livros 
-//     [5] Alugar Livro
-//     [6] Devolver Livro
-//     [7] Exportar Livros 
-//     [8] Importar Livros 
-//     [0] Sair
-//     --------------------------
-//     `);
+while(true){
+    console.log(`Biblioteca JS`);
+    console.log(`
+    --------------------------
+    [1] Adicionar Livro 
+    [2] Remover Livro 
+    [3] Atualizar Livro 
+    [4] Exibir Livros 
+    [5] Alugar Livro
+    [6] Devolver Livro
+    [7] Exportar Livros 
+    [8] Importar Livros 
+    [0] Sair
+    --------------------------
+    `);
 
-//     const opcao = parseInt(prompt("Digite a opcao desejada: "));
+    const opcao = parseInt(prompt("Digite a opcao desejada: "));
 
-//     if(opcao == 0){ break; }
-//     if(opcao == 1){ adicionarUsuario()}
-//     if(opcao == 2){ usuarios.length > 0 ? removerUsuario() : console.log("Nao existem usuarios cadastrados"); }
-//     if(opcao == 3){ usuarios.length > 0 ? atualizarUsuario() : console.log("Nao existem usuarios cadastrados");}
-//     if(opcao == 4){ usuarios.length > 0 ? visualizarUsuarios() : console.log("Nao existem usuarios cadastrados"); }
-//     if(opcao == 5){ usuarios.length > 0 ? salvarUsuarios() : console.log("Nao existem usuarios cadastrados");}
-//     if(opcao == 6){ carregarUsuarios(); }
-// }
+    if(opcao == 0){ break; }
+    if(opcao == 1){ adicionarLivro()}
+    if(opcao == 2){ livros.length > 0 ? removerLivro() : console.log("Nao existem livros cadastrados");}
+    if(opcao == 3){ livros.length > 0 ? atualizarLivro() : console.log("Nao existem livros cadastrados");}
+    if(opcao == 4){ livros.length > 0 ? visualizarLivros() : console.log("Nao existem livros cadastrados");}
+    if(opcao == 5){ livros.length > 0 ? alugarLivro() : console.log("Nao existem livros cadastrados");}
+    if(opcao == 6){ livros.length > 0 ? devolverLivro() : console.log("Nao existem livros cadastrados");}
+    if(opcao == 7){ livros.length > 0 ? salvarLivros() : console.log("Nao existem livros cadastrados");}
+    if(opcao == 8){ carregarLivros(); }
+}
 
-//
+
 // Funcoes auxiliares
 
 
 // Função para carregar livros de um arquivo JSON
-function carregarUsuarios() {
+function carregarLivros() {
     if (fs.existsSync('livros.json')) {
         const data = fs.readFileSync('livros.json', 'utf8');
         usuarios = JSON.parse(data);
@@ -50,7 +50,7 @@ function carregarUsuarios() {
 }
 
 // Função para salvar livros em um arquivo JSON
-function salvarUsuarios() {
+function salvarLivros() {
     const data = JSON.stringify(livros, null, 2);
     fs.writeFileSync('livros.json', data);
     console.log("Livros exportados com sucesso para 'usuarios.json'");
@@ -131,7 +131,7 @@ function visualizarLivros(disponiveis = true){
             if(livro.disponivel){
                 console.log(`
                 ============================================================
-                                Livros Disponiveis
+                                     Livros Disponiveis                                        
                 ============================================================
                 ID: ${livro.id}
                 TITULO: ${livro.titulo}
@@ -147,7 +147,7 @@ function visualizarLivros(disponiveis = true){
             if(livro.disponivel === false){
                 console.log(`
                 ============================================================
-                                Livros Disponiveis
+                                      Livros Alugados                                                             
                 ============================================================
                 ID: ${livro.id}
                 TITULO: ${livro.titulo}
@@ -163,7 +163,8 @@ function visualizarLivros(disponiveis = true){
 
 //funcao para devolver livro
 function devolverLivro(){
-    const livroADevolver = parseInt(prompt("ID da obra a ser devolvida: "));
+    visualizarLivros(false);
+    const livroADevolver = parseInt(prompt("ID da obra que deseja devolver: "));
     for(livro of livros){
         if(livro.id === livroADevolver && livro.disponivel === false){
             livro.disponivel = true;
