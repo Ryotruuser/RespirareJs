836ec775698e38db30a9bd2142e45ceb84d0c12c
Ryotruuser <ryonantruuser@gmail.com>
2024-06-01 19:17:47 -0300

    implementando funcao visualziar para exibir apenas obras disponiveis caso deseje

--- Challenges/biblioteca.js
+++ Challenges/biblioteca.js
@@ -5,6 +5,41 @@ let livros = [{id: 0, titulo: "Harry Potter", autor: "J.K ROWLING", ano: 1997, d
 {id: 1, titulo: "Harry Potter 2", autor: "J.K ROWLING", ano: 1997, disponivel: true},
 {id: 2, titulo: "Harry Potter 3", autor: "J.K ROWLING", ano: 1997, disponivel: false}];
 
+
+//loop do programa principal
+
+//loop do programa principal
+// while(true){
+//     console.log(`Biblioteca JS`);
+//     console.log(`
+//     --------------------------
+//     [1] Adicionar Livro 
+//     [2] Remover Livro 
+//     [3] Atualizar Livro 
+//     [4] Exibir Livros 
+//     [5] Alugar Livro
+//     [6] Devolver Livro
+//     [7] Exportar Livros 
+//     [8] Importar Livros 
+//     [0] Sair
+//     --------------------------
+//     `);
+
+//     const opcao = parseInt(prompt("Digite a opcao desejada: "));
+
+//     if(opcao == 0){ break; }
+//     if(opcao == 1){ adicionarUsuario()}
+//     if(opcao == 2){ usuarios.length > 0 ? removerUsuario() : console.log("Nao existem usuarios cadastrados"); }
+//     if(opcao == 3){ usuarios.length > 0 ? atualizarUsuario() : console.log("Nao existem usuarios cadastrados");}
+//     if(opcao == 4){ usuarios.length > 0 ? visualizarUsuarios() : console.log("Nao existem usuarios cadastrados"); }
+//     if(opcao == 5){ usuarios.length > 0 ? salvarUsuarios() : console.log("Nao existem usuarios cadastrados");}
+//     if(opcao == 6){ carregarUsuarios(); }
+// }
+
+//
+// Funcoes auxiliares
+
+
 // Função para carregar livros de um arquivo JSON
 function carregarUsuarios() {
     if (fs.existsSync('livros.json')) {
@@ -89,21 +124,43 @@ function atualizarLivro(){
 }
 
 // Funcao para exibir livro 
-function visualizarLivros(){
-    for(livro of livros){
-        console.log(`
-        ============================================================
-                           Livros Disponiveis
-        ============================================================
-        ID: ${livro.id}
-        TITULO: ${livro.titulo}
-        AUTOR: ${livro.autor}
-        ANO: ${livro.ano}
-        DISPONIVEL: ${livro.disponivel ? "Disponivel" : "Alugado"}
-        ============================================================
-        `);
-    };
+function visualizarLivros(disponiveis = false){
+
+    if(!disponiveis){
+        for(livro of livros){
+            console.log(`
+            ============================================================
+                               Livros Disponiveis
+            ============================================================
+            ID: ${livro.id}
+            TITULO: ${livro.titulo}
+            AUTOR: ${livro.autor}
+            ANO: ${livro.ano}
+            DISPONIVEL: ${livro.disponivel ? "Disponivel" : "Alugado"}
+            ============================================================
+            `);
+        };
+    }else{
+        for(livro of livros){
+            if(livro.disponivel === true){
+                console.log(`
+                ============================================================
+                                Livros Disponiveis
+                ============================================================
+                ID: ${livro.id}
+                TITULO: ${livro.titulo}
+                AUTOR: ${livro.autor}
+                ANO: ${livro.ano}
+                DISPONIVEL: ${livro.disponivel ? "Disponivel" : "Alugado"}
+                ============================================================
+                `);
+            }  
+        };
+    }
 }
+
+visualizarLivros()
+
 
 //funcao para devolver livro
 function devolverLivro(){
@@ -117,7 +174,6 @@ function devolverLivro(){
 }
 
 //Funcao alugar livro
-
 function alugarLivro(){
     visualizarLivros();
     const livroASerAlugado = parseInt(prompt("ID do livro que deseja alugar: "))
@@ -129,6 +185,4 @@ function alugarLivro(){
             console.log("Titulo indisponivel para aluguel.");
         }
     }
-
-
 }
