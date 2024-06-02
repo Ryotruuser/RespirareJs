b2d436c4e37e19c78a17a08d8affb67aab97d497
Ryotruuser <ryonantruuser@gmail.com>
2024-06-02 19:08:11 -0300

    sistema de votacao finalizado

---
+++ Challenges/sistemaVotacao.js
@@ -0,0 +1,114 @@
+const fs = require('fs');
+const prompt = require("prompt-sync")({ sigint: true }); 
+
+let candidatos = [];
+
+// Função para carregar Candidatos de um arquivo JSON
+function carregarCandidatos() {
+    if (fs.existsSync('candidatos.json')) {
+        const data = fs.readFileSync('candidatos.json', 'utf8');
+        candidatos = JSON.parse(data);
+        console.log("candidatos carregados com sucesso.");
+    }else {
+        console.log("Nenhum arquivo de candidatos encontrado.");
+    }
+    
+}
+
+// Função para salvar Candidatos em um arquivo JSON
+function salvarCandidatos() {
+    const data = JSON.stringify(candidatos, null, 2);
+    fs.writeFileSync('candidatos.json', data);
+    console.log("Candidatos exportados com sucesso para 'candidatos.json'");
+}
+
+function adicionarCandidato(){
+    const id = candidatos.length ? candidatos[candidatos.length - 1].id + 1 : 1;
+    const nome = prompt("Digite o nome: ");
+
+    const candidato = {id: id, nome: nome, votos: 0};
+    candidatos.push(candidato);
+    console.log("candidato cadastrado com sucesso.");
+}
+
+function removerCandidato(){
+    const id = parseInt(prompt("Digite o id do candidato a ser removido: "), 10);
+    const indice = candidatos.findIndex(candidato => candidato.id === id);
+    if (indice !== -1) {
+        candidatos.splice(indice, 1);
+        console.log("candidato removido com sucesso! ");
+    } else {
+        console.log("ID do candidato nao encontrado.");
+    }
+}
+
+function visualizarCandidatos(){
+    for(candidato of candidatos){
+        console.log(`
+        ID: ${candidato.id}
+        NOME: ${candidato.nome}
+        VOTOS: ${candidato.votos}
+        `);
+    };
+}
+
+function registrarVoto(){
+    const id = parseInt(prompt("Digite o id do candidato a ser votado: "), 10);
+    for(candidato of candidatos){
+        if(candidato.id === id){
+            if(candidato.votos >= 5){
+                exibirVencedor();
+            }else{
+                candidato.votos += 1;
+            }
+            
+        }
+    }
+}
+
+function exibirVencedor(){
+   for(candidato of candidatos){
+        if(candidato.votos >= 5){
+            console.log(`
+            ==============================
+                     VENCEDOR 
+            ==============================
+            ID: ${candidato.id}
+            NOME: ${candidato.nome}
+            VOTOS: ${candidato.votos}
+            ===============================
+            `);
+        }
+    }
+}
+
+
+//loop
+while(true){
+
+    console.log(`
+    --------------------------
+        Sistema de Votacao
+    --------------------------
+    [1] Adicionar candidato 
+    [2] Remover candidato 
+    [3] Realizar voto
+    [4] Exibir candidatos 
+    [5] Exibir vencedor
+    [6] Exportar candidatos
+    [7] Importar candidatos 
+    [0] Sair
+    --------------------------
+    `);
+
+    const opcao = parseInt(prompt("Digite a opcao desejada: "));
+
+    if(opcao == 0){ break; }
+    if(opcao == 1){ adicionarCandidato()}
+    if(opcao == 2){ candidatos.length > 0 ? removerCandidato() : console.log("Nao existem candidatos cadastrados"); }
+    if(opcao == 3){ candidatos.length > 0 ? registrarVoto() : console.log("Nao existem candidatos cadastrados");}
+    if(opcao == 4){ candidatos.length > 0 ? visualizarCandidatos() : console.log("Nao existem candidatos cadastrados"); }
+    if(opcao == 5){ candidatos.length > 0 ? exibirVencedor() : console.log("Nao existem candidatos cadastrados");}
+    if(opcao == 6){ candidatos.length > 0 ? salvarCandidatos() : console.log("Nao existem candidatos cadastrados");}
+    if(opcao == 7){ carregarCandidatos(); }
+}
