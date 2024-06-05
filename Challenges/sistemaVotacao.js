b3a71096ffc6e0be83adc2a948866d7bdd9bbd5f
Ryotruuser <ryonantruuser@gmail.com>
2024-06-05 20:04:38 -0300

    refatorando codigo sistema votacao com funcoes importadas

--- Challenges/sistemaVotacao.js
+++ Challenges/sistemaVotacao.js
@@ -1,33 +1,8 @@
 const fs = require('fs');
+const funcoesUteis = require('./funcoesUteis');
 const prompt = require("prompt-sync")({ sigint: true }); 
 
 let candidatos = [];
-
-// Função para carregar Candidatos de um arquivo JSON
-function carregarCandidatos() {
-    try {
-        if (fs.existsSync('candidatos.json')) {
-            const data = fs.readFileSync('candidatos.json', 'utf8');
-            candidatos = JSON.parse(data);
-            console.log("Candidatos carregados com sucesso.");
-        } else {
-            console.log("Nenhum arquivo de candidatos encontrado.");
-        }
-    } catch (error) {
-        console.error("Erro ao carregar candidatos:", error);
-    }
-}
-
-// Função para salvar Candidatos em um arquivo JSON
-function salvarCandidatos() {
-    try {
-        const data = JSON.stringify(candidatos, null, 2);
-        fs.writeFileSync('candidatos.json', data);
-        console.log("Candidatos exportados com sucesso para 'candidatos.json'");
-    } catch (error) {
-        console.error("Erro ao salvar candidatos:", error);
-    }
-}
 
 function adicionarCandidato(){
     const nome = prompt("Digite o nome: ").trim();
@@ -40,29 +15,6 @@ function adicionarCandidato(){
     candidatos.push(candidato);
     console.log("Candidato cadastrado com sucesso.");
 }
-
-
-function removerCandidato(){
-    const id = parseInt(prompt("Digite o id do candidato a ser removido: "), 10);
-    const indice = candidatos.findIndex(candidato => candidato.id === id);
-    if (indice !== -1) {
-        candidatos.splice(indice, 1);
-        console.log("candidato removido com sucesso! ");
-    } else {
-        console.log("ID do candidato nao encontrado.");
-    }
-}
-
-function visualizarCandidatos(){
-    candidatos.forEach(candidato => {
-        console.log(`
-        ID: ${candidato.id}
-        NOME: ${candidato.nome}
-        VOTOS: ${candidato.votos}
-        `);
-    });
-}
-
 
 function registrarVoto(){
     const id = parseInt(prompt("Digite o id do candidato a ser votado: "), 10);
@@ -78,7 +30,6 @@ function registrarVoto(){
         console.log("ID do candidato não encontrado.");
     }
 }
-
 
 function exibirVencedor(){
     let vencedor = false;
@@ -100,8 +51,6 @@ function exibirVencedor(){
         console.log("Nenhum candidato atingiu o número de votos necessários para vencer.");
     }
 }
-
-
 
 //loop
 while(true){
@@ -125,10 +74,10 @@ while(true){
 
     if(opcao == 0){ break; }
     if(opcao == 1){ adicionarCandidato()}
-    if(opcao == 2){ candidatos.length > 0 ? removerCandidato() : console.log("Nao existem candidatos cadastrados"); }
+    if(opcao == 2){ candidatos.length > 0 ? funcoesUteis.remover(candidatos) : console.log("Nao existem candidatos cadastrados"); }
     if(opcao == 3){ candidatos.length > 0 ? registrarVoto() : console.log("Nao existem candidatos cadastrados");}
-    if(opcao == 4){ candidatos.length > 0 ? visualizarCandidatos() : console.log("Nao existem candidatos cadastrados"); }
+    if(opcao == 4){ candidatos.length > 0 ? funcoesUteis.visualizar(candidatos) : console.log("Nao existem candidatos cadastrados"); }
     if(opcao == 5){ candidatos.length > 0 ? exibirVencedor() : console.log("Nao existem candidatos cadastrados");}
-    if(opcao == 6){ candidatos.length > 0 ? salvarCandidatos() : console.log("Nao existem candidatos cadastrados");}
-    if(opcao == 7){ carregarCandidatos(); }
+    if(opcao == 6){ candidatos.length > 0 ? funcoesUteis.salvar(candidatos, 'candidatos') : console.log("Nao existem candidatos cadastrados");}
+    if(opcao == 7){ funcoesUteis.carregar(candidatos, 'candidatos'); }
 }
