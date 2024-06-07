1d492afedfb2cb627463b485ad251b6af4d9a035
Ryotruuser <ryonantruuser@gmail.com>
2024-06-07 17:51:21 -0300

    criando funcao para identificar duplicadas a cada paragrafo

--- Learning/bibliotecaNode/src/index.js
+++ Learning/bibliotecaNode/src/index.js
@@ -3,8 +3,17 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-    verificaPalavrasDuplicadas(texto);
+    quebraEmParagrafos(texto);
+    // verificaPalavrasDuplicadas(texto);
 })
+
+
+
+function quebraEmParagrafos(texto){
+    const paragrafos = texto.toLowerCase().split("\n");
+    const contagem = paragrafos.map(paragrafo => verificaPalavrasDuplicadas(paragrafo));
+    console.log(contagem);
+}
 
 function verificaPalavrasDuplicadas(texto){
     const listaPalavras = texto.split(" ");
@@ -13,5 +22,5 @@ function verificaPalavrasDuplicadas(texto){
     listaPalavras.forEach(palavra => {
         resultado[palavra] = (resultado[palavra] || 0 ) + 1;
     });
-    console.log(resultado)
+    return resultado;
 }
