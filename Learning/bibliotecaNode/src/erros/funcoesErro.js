2116e9fdcac96a5ec4d0aa129893ed900555fcee
Ryotruuser <ryonantruuser@gmail.com>
2024-06-12 19:51:21 -0300

    compactando o codigo em modulos e exportando eles para o main file

--- Learning/bibliotecaNode/src/erros/funcoesErro.js
+++ Learning/bibliotecaNode/src/erros/funcoesErro.js
@@ -1,9 +1,7 @@
-function trataErros(erro){
+export default function trataErros(erro){
     if(erro.code === "ENOENT"){
         throw new Error("Arquivo nao encontrado.");
     }else{
         return "Erro na aplicacao";
     }
 }
-
-module.exports = trataErros;
--- Learning/bibliotecaNode/src/index.js
+++ Learning/bibliotecaNode/src/index.js
@@ -1,18 +1,4 @@
-const fs = require('fs');
-const trataErros = require('./erros/funcoesErro');
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
-
-fs.readFile(link, 'utf-8', (erro, texto) => {
-    try{    
-        if(erro) throw erro;
-        contarPalavras(texto);
-    }catch(erro){
-        trataErros(erro);
-  }
-})
-
-function contarPalavras(texto){
+export function contarPalavras(texto){
     const paragrafos = extraiParagrafos(texto);
     const contagem = paragrafos.flatMap((paragrafo) => {
         if(!paragrafo) return [];
