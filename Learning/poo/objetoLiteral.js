5637ad55a39742d362833d91c1949833c65e8de7
Ryotruuser <ryonantruuser@gmail.com>
2024-06-17 18:03:59 -0300

    entendendo heranca de prototipo

--- Learning/poo/objetoLiteral.js
+++ Learning/poo/objetoLiteral.js
@@ -2,11 +2,23 @@ const user = {
     nome: "Juliana",
     email: "j@j.com",
     nascimento: "2021/01/01",
-    role: "Admin",
+    role: "estudante",
     ativo: true,
     exibirInfo: function(){
         console.log(this.nome, this.email);
     }    
 }
 
-user.exibirInfo()
+const admin = {
+    nome: "Mariana",
+    email: "m@mcom",
+    role: "admin",
+    criarCurso (){
+        console.log("Curso criado");
+    }
+}
+
+
+Object.setPrototypeOf(admin, user); //heranca de prototipos
+admin.criarCurso()
+admin.exibirInfo()
