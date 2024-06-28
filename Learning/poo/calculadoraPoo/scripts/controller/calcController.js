fc23585a0f164123cc446dc223d8b4c06d2f83e3
Ryotruuser <ryonantruuser@gmail.com>
2024-06-28 15:58:09 -0300

    criando metodo para adicionar o ponto nas operacoes

--- Learning/poo/calculadoraPoo/scripts/controller/calcController.js
+++ Learning/poo/calculadoraPoo/scripts/controller/calcController.js
@@ -47,6 +47,8 @@ class CalcController{
     clearAll(){
 
         this.#operation = [];
+        this.#lastNumber = "";
+        this.#lastOperator = "";
 
         this.setLastNumberToDisplay();
     }
@@ -172,10 +174,6 @@ class CalcController{
 
                 this.setLastOperation(value);
 
-            }else if(isNaN(value)){
-
-                console.log(value);
-
             }else{
 
                 this.pushOperation(value);
@@ -192,7 +190,7 @@ class CalcController{
             }else{
 
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseInt(newValue));
+                this.setLastOperation(parseFloat(newValue));
 
                 this.setLastNumberToDisplay();
 
@@ -205,6 +203,25 @@ class CalcController{
     setError(){
 
         this.displayCalc = "Error";
+    }
+
+
+    addDot(){
+
+        let lastOperation = this.getLastOperation();
+        if(this.isOperator(lastOperation) || !lastOperation){
+
+            this.pushOperation("0.");
+
+        }else{
+
+            this.setLastOperation(lastOperation.toString() + ".");
+
+        }
+
+        this.setLastNumberToDisplay();
+
+
     }
 
     execBtn(value){
@@ -234,7 +251,7 @@ class CalcController{
                 this.calc();
                 break;
             case "ponto":
-                this.addOperation(".");
+                this.addDot(".");
                 break;
             
             case '0':
