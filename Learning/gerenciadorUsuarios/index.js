77376e74f880c98ab86bcfb6d5f1c55f9edfabf6
Ryotruuser <ryonantruuser@gmail.com>
2024-06-29 07:36:10 -0300

    pegando elementos do formulario com forEach

--- Learning/gerenciadorUsuarios/index.js
+++ Learning/gerenciadorUsuarios/index.js
@@ -1,2 +1,14 @@
-let nome = document.querySelector("#exempleInputName");
-nome.value
+// let name = document.querySelector("#exampleInputName");
+// let gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+// let birth = document.querySelector("#exampleInputBirth");
+// let country = document.querySelector("#exampleInputCountry");
+// let email = document.querySelector("#exampleInputEmail");
+// let password = document.querySelector("#exampleInputPassword");
+// let photo = document.querySelector("#exampleInputFile");
+// let admin = document.querySelector("#exampleInputAdmin");
+
+let fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach( field => {
+    console.log(field.value)
+})
