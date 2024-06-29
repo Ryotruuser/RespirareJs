c7513b66918731d41ffa2213cc333396f4a23171
Ryotruuser <ryonantruuser@gmail.com>
2024-06-29 08:14:26 -0300

    criando json dinamicamente

--- Learning/gerenciadorUsuarios/index.js
+++ Learning/gerenciadorUsuarios/index.js
@@ -1,14 +1,18 @@
-// let name = document.querySelector("#exampleInputName");
-// let gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-// let birth = document.querySelector("#exampleInputBirth");
-// let country = document.querySelector("#exampleInputCountry");
-// let email = document.querySelector("#exampleInputEmail");
-// let password = document.querySelector("#exampleInputPassword");
-// let photo = document.querySelector("#exampleInputFile");
-// let admin = document.querySelector("#exampleInputAdmin");
-
 let fields = document.querySelectorAll("#form-user-create [name]");
 
+let user = {};
+
 fields.forEach( field => {
-    console.log(field.value)
+
+    if(field.name == "gender"){
+        
+        if(field.checked){
+            user[field.name] = field.value;
+        }
+
+    }else{
+
+        user[field.name] = field.value;
+
+    }
 })
