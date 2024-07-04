e4407aca99f1acc564427b5acbd56c2f465f3da5
Ryotruuser <ryonantruuser@gmail.com>
2024-07-04 20:15:22 -0300

    criada a classe empresa e os metodos para contratar, demitir e listarfuncionarios, o metodo apresentar do funcionario agora recebe um parametro caso ele esteja atuado por uma empresa ou seja autonomo

--- Challenges/pagamentoFuncionario.js
+++ Challenges/pagamentoFuncionario.js
@@ -6,8 +6,11 @@ class Funcionario{
         this.saldo = saldo;
     }
 
-    apresentar(){
-        return `Ola me chamo ${this.nome} sou ${this.cargo} e atualmente ganho R$${this.salario} reais`;
+    apresentar(nomeEmpresa = false){
+        if(nomeEmpresa){
+            return `Ola me chamo ${this.nome} sou ${this.cargo} e atualmente ganho R$${this.salario} reais, atuo na empresa ${nomeEmpresa} \n`;
+        }
+        return `Ola me chamo ${this.nome} sou ${this.cargo} e atualmente ganho R$${this.salario} reais \n`;
     }
 
     depositar(valor){   
@@ -20,7 +23,43 @@ class Funcionario{
     }
 }
 
+class Empresa{
+    constructor(nome){
+        this.nome = nome;
+        this.funcionarios = [];
+    }
+
+    contratar(funcionario){
+        const contratado = this.funcionarios.find(funcionario => funcionario.nome === contratado.nome);
+        if(contratado){
+            return `Funcionario ja e nosso colaborador!`;
+        }
+        this.funcionarios.push(funcionario);
+        return `Bem vindo a nossa empresa ${funcionario.nome} !`;
+    }
+
+
+    demitir(funcionario){
+        const contratado = this.funcionarios.find(funcionario => funcionario.nome === contratado.nome);
+        if(contratado){
+            for(let contratados of this.funcionarios){
+                if(funcionario.nome == contratados.nome){
+                    let indexFuncionario = indexOf(contratados)
+                    this.funcionarios.splice(indexFuncionario, 1);
+                }
+            }
+        }
+    }
+
+    listarFuncionarios(){
+        return this.funcionarios.map(funcionario => funcionario.apresentar(this.nome)).join("\n");
+    }
+}
+
+const empresa = new Empresa("Xcom");
+
 const f1 = new Funcionario("Mauro", "Empresario", 5600);
-console.log(f1.apresentar())
-console.log(f1.depositar(5600))
-console.log(f1.consultarSaldo())
+console.log(f1.apresentar());
+
+console.log(empresa.contratar(f1));
+console.log(empresa.listarFuncionarios());
