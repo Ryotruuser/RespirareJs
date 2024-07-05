75bdb9e599999fa85755f11b25cdcfd08b24d48d
Ryotruuser <ryonantruuser@gmail.com>
2024-07-05 19:58:52 -0300

    criada classe humana e implementado metodos para incorporar a lista de pets

--- Challenges/petHotel.js
+++ Challenges/petHotel.js
@@ -11,11 +11,11 @@ class Pet{
 
     apresentar(){
         if(this.tipo == "cao"){
-            return `Au au sou o ${this.nome} um caozinho muito esperto de ${this.idade} anos, sou ${genero == "M" ? "menino" : "menina"}`;
+            return `Au au sou o ${this.nome} um caozinho muito esperto de ${this.idade} anos, sou ${this.genero == "M" ? "menino" : "menina"}`;
         }else if(this.tipo == "gato"){
-            return `Meow sou o ${this.nome} um gatinho muito agitado de ${this.idade} anos, sou ${genero == "M" ? "menino" : "menina"}`;
+            return `Meow sou o ${this.nome} um gatinho muito agitado de ${this.idade} anos, sou ${this.genero == "M" ? "menino" : "menina"}`;
         }else{
-            return `Brr sou o ${this.nome} um passaro muito sapeca de ${this.idade} anos, sou ${genero == "M" ? "menino" : "menina"}`;
+            return `Brr sou o ${this.nome} um passaro muito sapeca de ${this.idade} anos, sou ${this.genero == "M" ? "menino" : "menina"}`;
         }
     }
 
@@ -27,3 +27,25 @@ class Pet{
         this.sede += 1;
     }
 }
+
+class Pessoa{
+    constructor(nome, idade, pets = []){
+        this.nome = nome;
+        this.idade = idade;
+        this.pets = pets;
+    }
+
+    listarPets(){
+        this.pets.map(pet => {
+            pet.apresentar();
+        })
+    }
+
+
+    apresentar(){
+        return `Meu nome e ${this.nome} tenho ${this.idade} anos e essa e minha familia:\n${this.listarPets()}`;
+    }
+}
+
+const cao = new Pet("Tchutchucao", "cao", 2, "F");
+console.log(cao.apresentar());
