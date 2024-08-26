a3dc7e9e212094eebff9dfc07e9904fa74df7133
Ryotruuser <ryonantruuser@gmail.com>
2024-08-26 17:19:39 -0300

    o inicio do fim paradigmas de objetos em node

---
+++ Learning/poo/OOjs/objLiteral.js
@@ -0,0 +1,10 @@
+const user = {
+    nome: "Renan",
+    email: "r@r.com",
+    nascimento: "2024-26-08",
+    role: "estudante",
+    ativo: true,
+    exibirInfos: function(){
+        console.log(this.nome, this.email)
+    }
+}
