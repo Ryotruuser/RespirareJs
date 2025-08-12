8c3cfec9e2e465b9bfb011144840689b21ce5d04
Ryotruuser <ryonantruuser@gmail.com>
2025-08-12 18:58:19 -0300

    alguns outros comandos

---
+++ Learning/algJs/selectionSort.js
@@ -0,0 +1,14 @@
+import livros from './listaLivros.js';
+import menorValor from './menorValor.js';
+
+for (let atual = 0; atual < livros.length; atual++){
+    let menor = menorValor(livros, atual);
+
+    let livroAtual = livros[atual];
+    let livroMenorPreco = livros[menor];
+
+    livros[atual] = livroMenorPreco;
+    livros[menor = livroAtual];
+}
+
+console.log(livros);
